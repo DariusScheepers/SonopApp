c6fc5e125f0346011a0257910d1b314914048239
bbdnet1882 <Darius@bbd.co.za>
2019-11-12 13:52:19 +0200

    settings file formatted

--- src/pages/user/settings/settings.ts
+++ src/pages/user/settings/settings.ts
@@ -11,17 +11,17 @@ import { BedieningTableModel } from '../../../../functions/src/models/bediening-
 import { bedieningTables } from '../../../../functions/src/constants/bediening-tables.constant'
 
 @Component({
-  	selector: 'page-settings',
-  	templateUrl: 'settings.html',
+	selector: 'page-settings',
+	templateUrl: 'settings.html',
 })
 export class SettingsPage {
 
-	settings:any;
-	bedieningTableID:any;
-	semi:any;
-	emailAddress:any;
-	editPasswordMode:boolean = false;
-    tables: BedieningTableModel[] = bedieningTables;
+	settings: any;
+	bedieningTableID: any;
+	semi: any;
+	emailAddress: any;
+	editPasswordMode: boolean = false;
+	tables: BedieningTableModel[] = bedieningTables;
 	constructor(public navCtrl: NavController, public toastCtrl: ToastController, public navParams: NavParams, public global: GlobalProvider, public http: Http) {
 		this.settings = new FormGroup({
 			table: new FormControl(),
@@ -34,28 +34,23 @@ export class SettingsPage {
 		this.loadCurrentTable();
 	}
 
-	public loadCurrentTable()
-	{
+	public loadCurrentTable() {
 		let jsonSend: UserIdentificationModel = new UserIdentificationModel(this.global.myUsrID);
-		this.http.post('/getSettings', jsonSend).subscribe
-		(
-			(data) =>
-			{
+		this.http.post('/getSettings', jsonSend).subscribe(
+			(data) => {
 				let jsonResp: StudentModel = JSON.parse(data.text());
 
 				this.bedieningTableID = jsonResp.bedieningTable;
 				this.semi = jsonResp.isSemi;
-				this.emailAddress = jsonResp.email;	
+				this.emailAddress = jsonResp.email;
 			},
-			(error) =>
-			{
-				handleError(this.navCtrl,error,this.toastCtrl);
+			(error) => {
+				handleError(this.navCtrl, error, this.toastCtrl);
 			}
 		)
 	}
 
-	public updateInformation(value: any)
-	{
+	public updateInformation(value: any) {
 		let jsonSend: StudentUpdateModel = {
 			studentID: this.global.myUsrID,
 			bedieningTable: value.table,
@@ -63,54 +58,43 @@ export class SettingsPage {
 			email: value.email
 		}
 
-		if (this.editPasswordMode && value.newpassword != null || value.newpassword != "")
-		{
-			if (value.newpassword != value.confirmpassword)
-			{
-				presentToast(this.toastCtrl,"Please ensure that your passwords match.");
+		if (this.editPasswordMode && value.newpassword != null || value.newpassword != "") {
+			if (value.newpassword != value.confirmpassword) {
+				presentToast(this.toastCtrl, "Please ensure that your passwords match.");
 				return false;
 			}
 		}
-	
-		this.http.post('/updateSettings', jsonSend).subscribe
-		(
-			() =>
-			{			
-				presentToast(this.toastCtrl,"Updated Information!");
-				if (this.editPasswordMode)
-				{
+
+		this.http.post('/updateSettings', jsonSend).subscribe(
+			_ => {
+				presentToast(this.toastCtrl, "Updated Information!");
+				if (this.editPasswordMode) {
 					let jsonSend: StudentUpdatePasswordModel =
 					{
 						studentID: this.global.myUsrID,
 						oldPassword: value.oldpassword,
 						newPassword: value.newpassword
 					};
-					
-					this.http.post('/updatePassword',jsonSend).subscribe
-					(
-						(data) =>
-						{
+
+					this.http.post('/updatePassword', jsonSend).subscribe(
+						(data) => {
 							var jsonResp: SuccessResponseModel = JSON.parse(data.text());
-							if (jsonResp.success)
-							{
-								presentLongToast(this.toastCtrl,"Updated Password!");
+							if (jsonResp.success) {
+								presentLongToast(this.toastCtrl, "Updated Password!");
 								this.editPasswordMode = false;
 							}
-							else
-							{
-								presentToast(this.toastCtrl,"Old Password is incorrect. Please try again.");
+							else {
+								presentToast(this.toastCtrl, "Old Password is incorrect. Please try again.");
 								return false;
 							}
 						},
-						(error) =>
-						{
-							handleError(this.navCtrl,error,this.toastCtrl);
+						(error) => {
+							handleError(this.navCtrl, error, this.toastCtrl);
 						}
 					)
 				}
 			},
-			(error) =>
-			{
+			(error) => {
 				handleError(this.navCtrl, error, this.toastCtrl);
 			}
 		)
