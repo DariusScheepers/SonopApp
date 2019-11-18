a2081d6a2e97757efd1ddd225479a0d2dfaadca4
bbdnet1882 <Darius@bbd.co.za>
2019-11-18 08:27:46 +0200

    About to attempt on splash and icons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-//import { HomePage } from '../pages/home/home';
-//import { ListPage } from '../pages/list/list';
 import { LoginPage } from '../pages/login/login';
 import { AnnouncementsPage } from '../pages/user/announcements/announcements';
 import { WeekendPage } from '../pages/user/weekend/weekend';
